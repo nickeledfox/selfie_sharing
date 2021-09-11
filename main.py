efc68ef2a2319338cb7340cc83d4a00ce540f93a
mia-7-7 <81501711+mia-7-7@users.noreply.github.com>
2021-09-10 17:59:44 -0700

    able to take and save pictures

--- main.py
+++ main.py
@@ -2,18 +2,26 @@ from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
 from sharing import Sharing
+import time
 
 Builder.load_file('uix.kv')
 
 class CameraScreen(Screen):
     def start(self):
-        pass
+        self.ids.camera.play = True
+        self.ids.camera_btn.text = 'Stop'
+        self.ids.camera.texture = \
+            self.ids.camera._camera.texture
 
     def stop(self):
-        pass
+        self.ids.camera.play = False
+        self.ids.camera_btn.text = 'Play'
+        self.ids.camera.texture = None
 
     def capture(self):
-        pass
+        capture_time = time.strftime('%H.%M.%S-%Y:%m:%d')
+        file_name = capture_time + '.png'
+        self.ids.camera.export_to_png(file_name)
 
 
 class ImageScreen:
