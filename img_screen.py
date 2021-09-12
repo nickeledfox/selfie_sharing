2ae1f97e6cbd9a73abb8271660fb1908ce55ebac
mia-7-7 <81501711+mia-7-7@users.noreply.github.com>
2021-09-12 13:41:03 -0700

    refactored

--- img_screen.py
+++ img_screen.py
@@ -1,15 +1,19 @@
-from kivy.app import App
+import webbrowser
+
+from picture_sharer import FileSharer
+
 from kivy.core.clipboard import Clipboard
 from kivy.uix.screenmanager import Screen
 from kivy.lang import Builder
 
-from picture_sharer import FileSharer
-
 Builder.load_file('img_screen.kv')
 
 class ImageScreen(Screen):
+    exceptions_msg = 'Create a Link First'
+
     def create_link(self):
         # Generates filestack link
+        # App imported in main
         picture_path = App.get_running_app()\
             .root.ids.camera_screen.pictures_path
         file_sharer = FileSharer(filepath=picture_path)
@@ -20,4 +24,10 @@ class ImageScreen(Screen):
         try:
             Clipboard.copy(self.url)
         except:
-            self.ids.link.text = 'Create a Link First'
+            self.ids.link.text = self.exceptions_msg
+
+    def open_link(self):
+        try:
+            webbrowser.open(self.url)
+        except:
+            self.ids.link.text = self.exceptions_msg
